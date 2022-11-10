ee54b9120a0569614407cd616af54305305618b1
Mnesma <92695845+Mnesma@users.noreply.github.com>
2022-11-10 13:15:26 -0500

    fixed sillychild copypasta bug

--- src/commands/copypasta/copypasta-sillychild.js
+++ src/commands/copypasta/copypasta-sillychild.js
@@ -25,13 +25,13 @@ module.exports = {
   
   async execute(interaction) {
     const { options } = interaction;
-    const weak_class = options.getString('weak_class');
-    const strong_class = options.getString('strong_class');
-    const class_rank = options.getNumber('class_rank');
-    const class_diff = class_rank-1
+    const weakClass = options.getString('weak-class');
+    const strongClass = options.getString('strong-class');
+    const classRank = options.getNumber('class-rank');
+    const classRankDiff = Math.max(0, classRank - 1);
 
     interaction.reply(`
-Silly child, you've got a lot to learn in life. If you used modern technology you'll have access to something called Google. If you googled "class damage charts" you'll realize ${weak_class} is ranked ${class_rank}. Do you understand what that means? Probably not so allow me to use my knowledge of epsilon-delta and apply it to e=mc2 and boom I have come to the conclusion that there are ${class_diff} other classes infront of it. Lets take a moment and really take in what that means... theres ${class_diff} other mother fuckers stronger than you. Do you like that? Imagine having ${class_diff} mother fuckers smarter than you in your classroom. You like that? No? Make a ${strong_class} my son.
+Silly child, you've got a lot to learn in life. If you used modern technology you'll have access to something called Google. If you googled "class damage charts" you'll realize ${weakClass} is ranked ${classRank}. Do you understand what that means? Probably not so allow me to use my knowledge of epsilon-delta and apply it to e=mc2 and boom I have come to the conclusion that there are ${classRankDiff} other classes infront of it. Lets take a moment and really take in what that means... theres ${classRankDiff} other mother fuckers stronger than you. Do you like that? Imagine having ${classRankDiff} mother fuckers smarter than you in your classroom. You like that? No? Make a ${strongClass} my son.
     `);
   }
 }
