c6b60025bfd319cc31beb3823038750153eca949
Mnesma <92695845+Mnesma@users.noreply.github.com>
2022-11-16 20:45:16 -0500

    Fixed kidstalkingshit typo

--- src/commands/copypasta/copypasta-kidstalkingshit.js
+++ src/commands/copypasta/copypasta-kidstalkingshit.js
@@ -15,7 +15,7 @@ module.exports = {
     const name = options.getString('name');
 
     await interaction.reply(`
-i think its hilarious u kids talking shit about ${name}. u wouldnt say this shit to them at lan, tehy're jacked. not only that but ${name} wears the freshest clothes, eats at the chillest restaurants and hangs out with the hottest dudes. yall are pathetic lol
+i think its hilarious u kids talking shit about ${name}. u wouldnt say this shit to them at lan, they're jacked. not only that but ${name} wears the freshest clothes, eats at the chillest restaurants and hangs out with the hottest dudes. yall are pathetic lol
     `);
   },
 };
