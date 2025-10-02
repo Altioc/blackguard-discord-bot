0349caf798dd2d4fb81105c796a25b005bb7d925
Altioc <kyledmessner@gmail.com>
2025-10-02 17:59:52 -0400

    how about now?

--- src/commands/EmotePack/index.ts
+++ src/commands/EmotePack/index.ts
@@ -5,10 +5,10 @@ import { AuthorOf } from "../../models/ExecutePermission";
 import { BotCommand } from "../../types/BotCommand";
 import { List } from "./subcommands/List";
 import { Load } from "./subcommands/Load";
-
+console.log(__dirname);
 export const emotePackPath = path.join(
     __dirname,
-    "../../nico/emotePacks"
+    "../../../../nico/emotePacks"
 );
 
 const EmotePack: BotCommand = {
